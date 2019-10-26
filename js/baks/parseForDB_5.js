635b27003627f51b7b796cd79d8497d61796490c
Yaroslav Khaustov <yhaustov@hotmail.com>
2019-10-27 02:41:39 +0300

    1. Add HTML table for DB content visualization

--- js/baks/parseForDB_5.js
+++ js/baks/parseForDB_5.js
@@ -7,7 +7,10 @@ var olymps = [];
 request('https://olimpiada.ru/activities', function (error, response, html) {
   if (!error && response.statusCode == 200) 
   {
+    //console.log(html);
     var $ = cheerio.load(html);
+    var elems = $('#megalist');
+    //console.log(elems);
     var olympTables = $('.fav_olimp');
     for (var i = olympTables.length - 1; i >= 0; i--) 
     {
@@ -17,14 +20,14 @@ request('https://olimpiada.ru/activities', function (error, response, html) {
         continue;
       } 
 
-      var nameOlympAndDate = $(olympTables[i]).find('span.headline').text();
+      var nameOlympOrig = $(olympTables[i]).find('span.headline').text();
       var dateOfEvent = $(olympTables[i]).find('span.headline.red').text();
-      var nameOlymp = nameOlympAndDate.slice(0, nameOlympAndDate.indexOf(dateOfEvent));
+      var nameOlymp = nameOlympOrig.slice(0, nameOlympOrig.indexOf(dateOfEvent));
       var olympDesc = $(olympTables[i]).find('a.olimp_desc').text();
       var subjects = $(olympTables[i]).find('span.subject_tag').text();
       var schoolClasses = $(olympTables[i]).find('span.classes_dop').text();
       var ratingOlymp = $(olympTables[i]).find('span.pl_rating').text();
-    
+      //console.log('************************************************');
       var olymp = {
         name: nameOlymp,
         date: dateOfEvent,
@@ -34,9 +37,17 @@ request('https://olimpiada.ru/activities', function (error, response, html) {
         rating: ratingOlymp
       };
       olymps.push(olymp);
+
+      //console.log(olymp);
     }
     
     var json = JSON.stringify(olymps);
     fs.writeFile('./olympiads.json', json, 'utf8');
+    //console.log(json);
+
   }
 });
+
+
+
+//console.log($('.subject_tag').text());
